bbb9832a79c2a9d2016dbb9f002496c999416618
Kavya272 <150464976+Kavya272@users.noreply.github.com>
2026-06-24 15:52:17 +0530

    Add PL/SQL script to apply discount for seniors

---
+++ DeepSkilling/Week 1/PL_SQL_Exercises/Scenario1_Discount.sql
@@ -0,0 +1,25 @@
+BEGIN
+   FOR rec IN (
+      SELECT l.LoanID
+      FROM Loans l
+      JOIN Customers c
+      ON l.CustomerID = c.CustomerID
+      WHERE c.Age > 60
+   )
+   LOOP
+
+      UPDATE Loans
+      SET InterestRate = InterestRate - 1
+      WHERE LoanID = rec.LoanID;
+
+      DBMS_OUTPUT.PUT_LINE(
+      'Discount applied to Loan ID: '
+      || rec.LoanID);
+
+   END LOOP;
+
+   COMMIT;
+END;
+/
+
+
