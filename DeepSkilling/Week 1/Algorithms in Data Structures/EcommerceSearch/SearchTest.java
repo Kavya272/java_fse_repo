821f6f2e3d6a5f528d4b09305f70025edbfe1dde
Kavya272 <150464976+Kavya272@users.noreply.github.com>
2026-06-24 12:41:36 +0530

    Add files via upload

---
+++ DeepSkilling/Week 1/Algorithms in Data Structures/EcommerceSearch/SearchTest.java
@@ -0,0 +1,61 @@
+package com.cognizant.search;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SearchTest {
+
+    public static Product linearSearch(Product[] products, int id) {
+
+        for (Product p : products) {
+            if (p.productId == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static Product binarySearch(Product[] products, int id) {
+
+        int low = 0;
+        int high = products.length - 1;
+
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            if (products[mid].productId == id)
+                return products[mid];
+
+            else if (products[mid].productId < id)
+                low = mid + 1;
+
+            else
+                high = mid - 1;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Phone", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories")
+        };
+
+        Product result1 = linearSearch(products, 103);
+
+        System.out.println("Linear Search Result:");
+        System.out.println(result1);
+
+        Arrays.sort(products, Comparator.comparingInt(p -> p.productId));
+
+        Product result2 = binarySearch(products, 103);
+
+        System.out.println("\nBinary Search Result:");
+        System.out.println(result2);
+    }
+}
