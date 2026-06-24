a804b25eab6520514cca5c1bab07eee2ce849f82
Kavya272 <150464976+Kavya272@users.noreply.github.com>
2026-06-24 21:05:19 +0530

    Add SQL script to insert account details

---
+++ DeepSkilling/Week 1/PL_SQL_Exercises/InsertAccountDetails.sql
@@ -0,0 +1,5 @@
+INSERT INTO Accounts VALUES (101,'Kavya','Savings',10000);
+INSERT INTO Accounts VALUES (102,'Ravi','Savings',20000);
+INSERT INTO Accounts VALUES (103,'Priya','Current',15000);
+
+COMMIT;
